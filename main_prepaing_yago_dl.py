6d6ca9c81e609ad3bbb18259864a72b0724f1edd
Riccardo Cappuzzo <riccardo.cappuzzo@gmail.com>
2023-04-11 10:39:29 +0200

    Moved most of the code to utils, added docstrings.

--- main_prepaing_yago_dl.py
+++ main_prepaing_yago_dl.py
@@ -1,181 +1,30 @@
-#%%
-import src.yago.utils as utils
-import polars as pl
-from pathlib import Path
-import numpy as np
-from tqdm import tqdm
 import re
+from pathlib import Path
 
+import src.yago.utils as utils
 
-#%%
-yago_path = Path("/storage/store3/work/jstojano/yago3/")
-facts1_path = Path(yago_path, "facts_parquet/yago_updated_2022_part1")
-facts2_path = Path(yago_path, "facts_parquet/yago_updated_2022_part2")
-#%%
-fname = "yagoTypes"
-yagotypes_path = Path(facts1_path, f"{fname}.tsv.parquet")
-yagotypes = utils.import_from_yago(yagotypes_path, engine="polars")
+if __name__ == "__main__":
 
-fname = "yagoFacts"
-yagofacts_path = Path(facts2_path, f"{fname}.tsv.parquet")
-yagofacts = utils.import_from_yago(yagofacts_path, engine="polars")
+    dest_path = Path("data/yago3-dl/seltab")
+    output_format = "parquet"
 
-fname = "yagoLiteralFacts"
-yagoliteralfacts_path = Path(facts2_path, f"{fname}.tsv.parquet")
-yagoliteralfacts = utils.import_from_yago(yagoliteralfacts_path, engine="polars")
+    yagofacts, yagotypes = utils.read_yago_files()
+    subject_count_sorted = utils.get_subject_count_sorted(yagofacts)
 
-fname = "yagoDateFacts"
-yagodatefacts_path = Path(facts2_path, f"{fname}.tsv.parquet")
-yagodatefacts = utils.import_from_yago(yagodatefacts_path, engine="polars")
-
-# %%
-yagofacts_overall = pl.concat(
-    [
-        yagofacts,
-        yagoliteralfacts,
-        yagodatefacts
-    ]
-)
-# %%
-subject_count_sorted=(yagofacts_overall.lazy().groupby(
-    "subject"
-).agg(
-    pl.count()
-).sort(
-    "count", descending=True
-    ).collect())
-# %%
-selected_types=subject_count_sorted.lazy().limit(
-    10000 # Taking the top 10000 subjects
-    ).join(
-    yagotypes.lazy(), on="subject" # Joining on types table
-    ).select( # Selecting only subject and type
-    [
-        pl.col("subject"),
-        pl.col("cat_object")
-    ]
-).join( # Joining again on yagotypes to get the count of subjects by type
-    yagotypes.groupby( # Grouping to get the count of subjects by type
-        "cat_object"
-    ).agg(
-        pl.count() # Count of subjects by type
-    ).lazy(), on="cat_object"
-).sort( # Sorting to have the most frequent types at the start
-    ["subject", "count"], descending=True
-).groupby(["subject"]).agg( # Grouping by subject to have all types close to each other
-    pl.first("cat_object").suffix("_first") # Selecting only the first entry from each group (i.e. the most frequent)
-    ).groupby( # At this point the selection is done
-        "cat_object_first" # Grouping by type to remove duplicates
-        ).agg(
-            pl.count() # counting the number of occurrences of each type
-        ).sort( # Sorting by group (for convenience)
-            "count", descending=True
-            ).select(
-                [
-                    pl.col("cat_object_first").alias("type"),
-                    pl.col("count")
-                ],
-            ).collect()
-
-# %%
-
-top_selected = selected_types.filter(
-    pl.col("count") > 10
-)
-
-subjects_in_selected_types = yagofacts_overall.lazy().join(top_selected.lazy().join(
-    yagotypes.lazy(), left_on=["type"], right_on=["cat_object"]
-).select(
-    [
-        pl.col("subject"),
-        pl.col("type")
-    ]
-), left_on="subject", right_on="subject"
-).collect()
-# %%
-types_predicates_selected = utils.join_types_predicates(yagotypes, yagofacts_overall, types_subset=top_selected)
-
-# %%
-adj_dict = {}
-for row in types_predicates_selected.iter_rows():
-    left, right, _ = row
-    if left not in adj_dict:
-        adj_dict[left] = [right]
-    else:
-        adj_dict[left].append(right)
-
-for k, v in adj_dict.items():
-    adj_dict[k] = set(v)
-
-# %%
-def convert_df(df: pl.DataFrame, predicate):
-    return df.select(
-        pl.col("subject"),
-        pl.col("cat_object").alias(predicate)
-    ).lazy()
-# %%
-type_groups = (yagotypes.lazy().join(
-    selected_types.head(10).lazy(), 
-    left_on="cat_object", right_on="type"
-    ).select(
-        [
-            pl.col("subject"),
-            pl.col("cat_object")
-        ]
-    ).groupby("cat_object").all().select(
-        [
-            pl.col("cat_object"),
-            pl.col("subject"),
-        ]
-        ).collect())
-
-# %%
-tabs_by_type = {}
-for tup in type_groups.iter_rows():
-    type_str, values = tup
-    tab = pl.DataFrame(
-        {
-        "type": [type_str]*len(values),
-        "subject": values
-        },
+    selected_types = utils.get_selected_types(
+        subject_count_sorted, yagotypes, n_subjects=10000
     )
-    tabs_by_type[type_str]=tab
 
-# %%
-dest_path = Path("data/yago3-dl/seltab")
+    subjects_in_selected_types, types_subset = utils.get_subjects_in_selected_types(
+        yagofacts, selected_types, yagotypes, min_count=10
+    )
 
-# %%
-def clean_keys(type_name):
-    pattern = r"(<)([a-zA-Z_]+)[_0-9]*>"
-    replacement = r"\g<2>"
-    return re.sub(pattern, replacement, type_name).rstrip("_")
+    types_predicates_selected = utils.join_types_predicates(
+        yagotypes, yagofacts, types_subset=types_subset
+    )
 
+    adj_dict = utils.build_adj_dict(types_predicates_selected)
 
-# %%
+    tabs_by_type = utils.get_tabs_by_type(yagotypes, selected_types)
 
-output_format = "parquet"
-
-for type_str, tab in tqdm(tabs_by_type.items(), total=len(tabs_by_type)):
-    new_tab = tab.clone()
-    tqdm.write(type_str)
-    for pred_name, pred_group in yagofacts.groupby("predicate"):
-        if pred_name in adj_dict[type_str] and len(new_tab.columns) < 15:
-        # if pred_name in G.neighbors(type_str) and pred_name in count_facts[:15]["predicate"]:
-            transformed_tab = convert_df(pred_group, pred_name)
-            new_tab= new_tab.lazy().join(
-                transformed_tab.lazy(),
-                on="subject",
-                how="left"
-            )
-    
-    clean_type_str = clean_keys(type_str)
-    if output_format == "csv":
-        fname = f"yago_seltab_{clean_type_str}.csv"
-        new_tab.collect().write_csv(Path(dest_path, fname))
-    elif output_format == "parquet":
-        fname = f"yago_seltab_{clean_type_str}.parquet"
-        new_tab.collect().write_parquet(Path(dest_path, fname))
-    else:
-        raise ValueError(f"Unkown output format {output_format}")
-
-# %%
+    utils.save_tabs_on_file(tabs_by_type, adj_dict, yagofacts, dest_path, output_format)
--- src/yago/utils.py
+++ src/yago/utils.py
@@ -1,14 +1,11 @@
+import re
 from pathlib import Path
 from typing import Union
 
-import matplotlib.pyplot as plt
-import networkx as nx
-import numpy as np
 import pandas as pd
-import plotly.graph_objects as go
 import polars as pl
 import seaborn as sns
-from matplotlib.colors import LogNorm
+from tqdm import tqdm
 
 
 def import_from_yago(filepath: Path, engine="polars"):
@@ -93,135 +90,16 @@ def count_occurrences_by_columns(
         raise TypeError("Inappropriate dataframe type.")
 
 
-# Function used to prepare the node layout for the graph picture.
-def bipartite(left, right, aspect_ratio=4 / 3, scale=1):
-    left = list(set(left))
-    right = list(set(right))
-    aspect_ratio = 1
-    height = 1
-    width = aspect_ratio * height
-    nodes = left + right
-    # offset = (width / 2, height / 2)
-    offset = 0
-
-    left_xs = np.repeat(0, len(left))
-    right_xs = np.repeat(width, len(right))
-    left_ys = np.linspace(0, height, len(left))
-    right_ys = np.linspace(0, height, len(right))
-
-    top_pos = np.column_stack([left_xs, left_ys]) - offset
-    bottom_pos = np.column_stack([right_xs, right_ys]) - offset
-
-    pos = np.concatenate([top_pos, bottom_pos])
-    dict_pos = dict(zip(nodes, pos))
-    return dict_pos
-
-
-def plot_graph(graph, left, right, image_path="", title="", width=800, height=600):
-    coord_list = bipartite(left, right)
-
-    edge_trace = get_edge_trace(graph, coord_list)
-
-    node_trace = get_node_trace(graph, coord_list)
-
-    node_text = []
-    ns = list(graph.nodes())
-
-    node_colors, node_text = get_node_info(graph, node_text)
-
-    node_trace.marker.color = node_colors
-    node_trace.text = node_text
-
-    fig = go.Figure(
-        data=[edge_trace, node_trace],
-        layout=go.Layout(
-            title=title,
-            titlefont_size=16,
-            showlegend=False,
-            hovermode="closest",
-            margin=dict(b=20, l=5, r=5, t=15),
-            xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
-            yaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
-            width=width,
-            height=height,
-        ),
-    )
-    fig.show()
-    if image_path:
-        fig.write_html("{}".format(image_path))
-
-
-def get_node_info(graph, node_text):
-    node_adjacencies = []
-    node_text = []
-    for node, adjacencies in enumerate(graph.adjacency()):
-        node_adjacencies.append(len(adjacencies[1]))
-        node_text.append(
-            f"{adjacencies[0]}: # of connections: " + str(len(adjacencies[1]))
-        )
-
-    return node_adjacencies, node_text
-
-
-def get_node_trace(graph, coord_list):
-    node_x = []
-    node_y = []
-    for node in graph.nodes():
-        x, y = coord_list[node]
-        node_x.append(x)
-        node_y.append(y)
-
-    node_trace = go.Scatter(
-        x=node_x,
-        y=node_y,
-        mode="markers",
-        hoverinfo="text",
-        #     text=node_text,
-        marker=dict(
-            showscale=True,
-            # colorscale="Blackbody",
-            reversescale=False,
-            color=[],
-            size=10,
-            colorbar=dict(
-                thickness=15,
-                title="Number of connections",
-                xanchor="left",
-                titleside="right",
-                # tickvals=[-1, 1, 0],
-                # ticktext=["RID", "CID", "Node"],
-            ),
-            line_width=2,
-        ),
-    )
-
-    return node_trace
-
-
-def get_edge_trace(graph, ll):
-    edge_x = []
-    edge_y = []
-    for edge in graph.edges:
-        x0, y0 = ll[edge[0]]
-        x1, y1 = ll[edge[1]]
-        edge_x.append(x0)
-        edge_x.append(x1)
-        edge_x.append(None)
-        edge_y.append(y0)
-        edge_y.append(y1)
-        edge_y.append(None)
-
-    edge_trace = go.Scatter(
-        x=edge_x,
-        y=edge_y,
-        line=dict(width=0.5, color="#888"),
-        hoverinfo="none",
-        mode="lines",
-    )
-    return edge_trace
-
-
 def get_cooccurring_predicates(df: pl.DataFrame):
+    """Given the df, perform a self-join, then return the predicate columns without
+    performing any aggregation.
+
+    Args:
+        df (pl.DataFrame): Yago-like dataframe to operate on.
+
+    Returns:
+        pl.DataFrame: A Dataframe that contains all the pairs of predicates without aggregation.
+    """
     return (
         df.lazy()
         .join(df.lazy(), left_on="subject", right_on="subject", how="left")
@@ -240,136 +118,274 @@ def get_count_cooccurring_predicates(df: pl.DataFrame):
     )
 
 
-def get_numerical_coordinates(df:pd.DataFrame):
-    df.columns = [
-        "pred_left",
-        "pred_right",
-        "count"
-    ]
-    df[
-        ["pred_left", "pred_right"]
-    ] = df[["pred_left", "pred_right"]].astype(
-        "category"
-    )
-    df[
-        ["pred_left_int", "pred_right_int"]
-    ] = df[["pred_left", "pred_right"]].apply(
-        lambda x: x.cat.codes
-    )
-    
-    return df
-
-
-def get_labels_from_cat_codes(df: pd.DateOffset):
-    labels_dict = dict(
-    sorted(
-        dict(
-            zip(
-                df["pred_left"].cat.codes,
-                df["pred_left"],
-            )
-        ).items()
-    )
-    )
-    return labels_dict
-    
-
-def plot_pairwise_heatmap(df_count_cooccurring_predicates: pd.DataFrame):
-    df_count_cooccurring_predicates = get_numerical_coordinates(df_count_cooccurring_predicates)
-    
-    max_category = (
-        df_count_cooccurring_predicates[["pred_left_int", "pred_right_int"]]
-        .max()
-        .max()
-    )
-    
-    # Set the proper coordinates
-    zz = np.zeros((max_category + 1, max_category + 1))
-    zz[
-        df_count_cooccurring_predicates["pred_left_int"],
-        df_count_cooccurring_predicates["pred_right_int"],
-    ] = df_count_cooccurring_predicates["count"]
-    # Set the proper labels
-    dd = get_labels_from_cat_codes(df_count_cooccurring_predicates)
-    
-    norm = LogNorm()
-    fig, ax = plt.subplots(figsize=(10,10))
-    sns.heatmap(
-        zz,
-        robust=True,
-        cmap="rocket",
-        norm=norm,
-        xticklabels=list(dd.values()),
-        yticklabels=list(dd.values()),
-        square=True,
-        ax=ax
-    )
-    ax.set_title("Frequency of co-occurrence of predicate pairs in frequent values")
-
-    return ax
-
-def plot_predicate_frequency(df_cooc_predicates: pd.DataFrame, ax=None, label=None):
-    if ax is not None:
-        sns.lineplot(data=df_cooc_predicates, x=range(len(df_cooc_predicates)),y="count", label=label, ax=ax)
-    else:
-        ax = sns.lineplot(data=df_cooc_predicates, x=range(len(df_cooc_predicates)),y="count", label=label)
-        
-    ax.set_yscale("log")
-    ax.set_title("Frequency of each pair of predicates. ")
-    ax.set_xlabel("Rank of the predicate pair.")
-    ax.set_ylabel("Number of occurrences of the predicate.")
-    return ax
-    
-def plot_pairwise_relplot(df_count_cooccurring_predicates):
-    df_count_cooccurring_predicates = get_numerical_coordinates(df_count_cooccurring_predicates)
-    
-    dd = get_labels_from_cat_codes(df_count_cooccurring_predicates)
-    
-    norm = LogNorm()
-    g = sns.relplot(
-        data=df_count_cooccurring_predicates,
-        x="pred_left_int",
-        y="pred_right_int",
-        size="count",
-        hue="count",
-        hue_norm=norm,
-        sizes=(10,250),
-        size_norm=norm, 
-        palette="vlag",
-        height=7
+def join_types_predicates(yagotypes, yagofacts, types_subset):
+    types_predicates = (
+        yagotypes.lazy()
+        .filter(pl.col("cat_object").is_in(types_subset["type"]))
+        .join(yagofacts.lazy(), left_on="subject", right_on="subject", how="left")
+        .select(
+            pl.col("subject"),
+            pl.col("cat_object").alias("type"),
+            pl.col("predicate_right").alias("predicate"),
         )
-    g.set(xticks=np.arange(len(dd)), yticks=np.arange(len(dd)))
-    _=g.set_xticklabels(list(dd.values()), rotation=45, horizontalalignment='right', fontsize=6)
-    _=g.set_yticklabels(list(dd.values()), horizontalalignment='right', fontsize=6)
-    sns.despine(left=True, bottom=True)
-    
-def join_types_predicates(yagotypes, yagofacts, types_subset):    
-    types_predicates=(yagotypes.lazy().filter(
-        pl.col("cat_object").is_in(types_subset["type"])
-    ).join(
-        yagofacts.lazy(),
-        left_on="subject",
-        right_on="subject",
-        how="left"
-    ).select(
-        pl.col("subject"),
-        pl.col("cat_object").alias("type"),
-        pl.col("predicate_right").alias("predicate")
-    ).unique(
-        ).drop_nulls(
-            ).select(
-                [
-                    pl.col("type"),
-                    pl.col("predicate")
-                ]
-            ).groupby(
-                [
-                    pl.col("type"),
-                    pl.col("predicate")
-                ]
-            ).agg(
-                [
-                    pl.count()
-                ]
-            ).sort("count", descending=True).collect())
+        .unique()
+        .drop_nulls()
+        .select([pl.col("type"), pl.col("predicate")])
+        .groupby([pl.col("type"), pl.col("predicate")])
+        .agg([pl.count()])
+        .sort("count", descending=True)
+        .collect()
+    )
     return types_predicates
+
+
+def read_yago_files():
+    yago_path = Path("/storage/store3/work/jstojano/yago3/")
+    facts1_path = Path(yago_path, "facts_parquet/yago_updated_2022_part1")
+    facts2_path = Path(yago_path, "facts_parquet/yago_updated_2022_part2")
+
+    fname = "yagoTypes"
+    yagotypes_path = Path(facts1_path, f"{fname}.tsv.parquet")
+    yagotypes = import_from_yago(yagotypes_path, engine="polars")
+
+    fname = "yagoFacts"
+    yagofacts_path = Path(facts2_path, f"{fname}.tsv.parquet")
+    yagofacts = import_from_yago(yagofacts_path, engine="polars")
+
+    fname = "yagoLiteralFacts"
+    yagoliteralfacts_path = Path(facts2_path, f"{fname}.tsv.parquet")
+    yagoliteralfacts = import_from_yago(yagoliteralfacts_path, engine="polars")
+
+    fname = "yagoDateFacts"
+    yagodatefacts_path = Path(facts2_path, f"{fname}.tsv.parquet")
+    yagodatefacts = import_from_yago(yagodatefacts_path, engine="polars")
+
+    yagofacts_overall = pl.concat([yagofacts, yagoliteralfacts, yagodatefacts])
+
+    return yagofacts_overall, yagotypes
+
+
+def get_subject_count_sorted(yagofacts: pl.DataFrame):
+    subject_count_sorted = (
+        yagofacts.lazy()
+        .groupby("subject")
+        .agg(pl.count())
+        .sort("count", descending=True)
+        .collect()
+    )
+    return subject_count_sorted
+
+
+def get_selected_types(
+    subject_count_sorted: pl.DataFrame, yagotypes: pl.DataFrame, n_subjects=10000
+):
+    selected_types = (
+        subject_count_sorted.lazy()
+        .limit(n_subjects)  # Taking the top 10000 subjects
+        .join(yagotypes.lazy(), on="subject")  # Joining on types table
+        .select(  # Selecting only subject and type
+            [pl.col("subject"), pl.col("cat_object")]
+        )
+        .join(  # Joining again on yagotypes to get the count of subjects by type
+            yagotypes.groupby(  # Grouping to get the count of subjects by type
+                "cat_object"
+            )
+            .agg(pl.count())  # Count of subjects by type
+            .lazy(),
+            on="cat_object",
+        )
+        .sort(  # Sorting to have the most frequent types at the start
+            ["subject", "count"], descending=True
+        )
+        .groupby(["subject"])
+        .agg(  # Grouping by subject to have all types close to each other
+            pl.first("cat_object").suffix(
+                "_first"
+            )  # Selecting only the first entry from each group (i.e. the most frequent)
+        )
+        .groupby(  # At this point the selection is done
+            "cat_object_first"  # Grouping by type to remove duplicates
+        )
+        .agg(pl.count())  # counting the number of occurrences of each type
+        .sort("count", descending=True)  # Sorting by group (for convenience)
+        .select(
+            [pl.col("cat_object_first").alias("type"), pl.col("count")],
+        )
+        .collect()
+    )
+    return selected_types
+
+
+def filter_selected_types(selected_types: pl.DataFrame, min_count):
+    top_selected = selected_types.filter(pl.col("count") > min_count)
+    return top_selected
+
+
+def get_subjects_in_selected_types(
+    yagofacts, selected_types, yagotypes, min_count: int = 10
+):
+
+    top_selected = filter_selected_types(selected_types, min_count=min_count)
+
+    subjects_in_selected_types = (
+        yagofacts.lazy()
+        .join(
+            top_selected.lazy()
+            .join(yagotypes.lazy(), left_on=["type"], right_on=["cat_object"])
+            .select([pl.col("subject"), pl.col("type")]),
+            left_on="subject",
+            right_on="subject",
+        )
+        .collect()
+    )
+
+    return subjects_in_selected_types, top_selected
+
+
+def build_adj_dict(types_predicates):
+    """Build an adjacency dictionary whose keys are the types, and each 
+    value is a list of all the predicates that are connected to the type through
+    some subject.
+
+    Args:
+        types_predicates (pl.DataFrame): Dataframe that contains the number of type-predicate pairs. 
+
+    Returns:
+        dict: Dictionary with pairs type-predicates. 
+    """
+    adj_dict = {}
+    for row in types_predicates.iter_rows():
+        left, right, _ = row
+        if left not in adj_dict:
+            adj_dict[left] = [right]
+        else:
+            adj_dict[left].append(right)
+
+    for k, v in adj_dict.items():
+        adj_dict[k] = set(v)
+
+    return adj_dict
+
+
+def convert_df(df: pl.DataFrame, predicate: str):
+    """Convert dataframe from triplet format to binary format. Given a dataframe
+    and a predicate, return a new dataframe where subjects are index keys, the
+    predicate is the attribute and the objects fill the attribute.
+
+    Args:
+        df (pl.DataFrame): Dataframe to convert.
+        predicate (str): Name of the predicate.
+
+    Returns:
+        pl.DataFrame: Converted dataframe.
+    """
+    return df.select(pl.col("subject"), pl.col("cat_object").alias(predicate)).lazy()
+
+
+def get_tabs_by_type(yagotypes, selected_types, group_limit=10):
+    """This function takes as input the types dataframe, the selected types and an optional
+    limit on the number of types to select and produces the starting tables that
+    will be used later to create the full DL tables.
+
+    Args:
+        yagotypes (pl.DataFrame): Yago types dataframe.
+        selected_types (pl.DataFrame): Dataframe with the selected types.
+        group_limit (int, optional): Number of types to use. Defaults to 10.
+
+    Raises:
+        ValueError: Raise ValueError if `group_limit` is < 1.
+
+    Returns:
+        dict[pl.DataFrame]: Dictionary of dataframes.
+    """
+    if group_limit < 1:
+        raise ValueError(f"`group_limit` must be > 1, got {group_limit}")
+
+    type_groups = (
+        yagotypes.lazy()
+        .join(
+            selected_types.head(group_limit).lazy(),  # Inner join selected types
+            left_on="cat_object",
+            right_on="type",
+        )
+        .select(
+            [pl.col("subject"), pl.col("cat_object")]
+        )  # select only subject and type
+        .groupby("cat_object")  # Group by type
+        .all()
+        .select(
+            [
+                pl.col("cat_object"),
+                pl.col("subject"),
+            ]
+        )
+        .collect()  #
+    )
+
+    tabs_by_type = {}
+    for tup in type_groups.iter_rows():
+        type_str, values = tup
+        tab = pl.DataFrame(
+            {"type": [type_str] * len(values), "subject": values},
+        )
+        tabs_by_type[type_str] = tab
+
+    return tabs_by_type
+
+
+def clean_keys(type_name):
+    """Utility function that removes unnecessary symbols in the Yago type names.
+
+    Args:
+        type_name (str): String to be reformatted.
+
+    Returns:
+        str: Reformatted string.
+    """
+    pattern = r"(<)([a-zA-Z_]+)[_0-9]*>"
+    replacement = r"\g<2>"
+    return re.sub(pattern, replacement, type_name).rstrip("_")
+
+
+def save_tabs_on_file(
+    tabs_by_type,
+    adj_dict,
+    yagofacts: pl.DataFrame,
+    dest_path,
+    output_format: str = "parquet",
+    max_table_width: int = 15,
+):
+    """Save the required tables in separate files with the given output format.
+
+    Args:
+        tabs_by_type (dict): Dictionary that contains the "base tables" to be joined on.
+        adj_dict (dict): Dictionary that lists predicates that co-occur, to avoid empty joins.
+        yagofacts (pl.DataFrame): Yago facts dataframe.
+        dest_path (_type_): Root folder where all tables will be saved.
+        output_format (str, optional): Output format to use, either "parquet" or "csv". Defaults to "parquet".
+        max_table_width (int, optional): Maximum width of the final table. Large tables can run out of memory. Defaults to 15.
+
+    Raises:
+        ValueError: Raise ValueError if the output format is not known.
+    """
+    for type_str, tab in tqdm(tabs_by_type.items(), total=len(tabs_by_type)):
+        new_tab = tab.clone()
+        tqdm.write(type_str)
+        for pred_name, pred_group in yagofacts.groupby("predicate"):
+            if (
+                pred_name in adj_dict[type_str]
+                and len(new_tab.columns) < max_table_width
+            ):
+                transformed_tab = convert_df(pred_group, pred_name)
+                new_tab = new_tab.lazy().join(
+                    transformed_tab.lazy(), on="subject", how="left"
+                )
+        clean_type_str = clean_keys(type_str)
+        if output_format == "csv":
+            fname = f"yago_seltab_{clean_type_str}.csv"
+            new_tab.collect().write_csv(Path(dest_path, fname))
+        elif output_format == "parquet":
+            fname = f"yago_seltab_{clean_type_str}.parquet"
+            new_tab.collect().write_parquet(Path(dest_path, fname))
+        else:
+            raise ValueError(f"Unkown output format {output_format}")
